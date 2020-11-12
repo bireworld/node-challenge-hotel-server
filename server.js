29450a8e047d9929f3779195f21a586748deb6d9
bireworld <mail2berhane@gmail.com>
2020-11-12 07:45:06 +0000

    new booking created

--- server.js
+++ server.js
@@ -14,6 +14,33 @@ app.get("/", function (request, response) {
 });
 
 // TODO add your routes and helper functions here
+// Reading all Bookings
+app.get("/bookings", (req,res)=>{
+  res.send(bookings)
+})
+
+//New booking Created
+app.post('/bookings',(req,res)=>{
+  
+   if(req.body.text || req.body.title || req.body.firstName || req.body.surname || req.body.email || req.body.roomId || req.body.checkInDate  ){
+  bookings.id = bookings.length
+
+  const newBooking = req.body
+  newBooking.id=bookings.length + 1
+     req.body.id = newBooking.id;
+      //req.body.timeSent = new Date ();
+      bookings.push (req.body);
+      res
+        .status (201)
+        .json (` Successfully A new booking with Id number ${newBooking.id}  is created.`);
+ 
+   }
+  else{
+    res.status(400).send("Bad Request")
+  }     
+     });
+
+
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
